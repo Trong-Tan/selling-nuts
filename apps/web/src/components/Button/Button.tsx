bc77492665446a496b9bdfedc2695eb2c9eb2546
Trong-Tan <trongtan.ttt0503@gmail.com>
2024-05-10 15:47:09 +0700

    add useState to Home page

--- apps/web/src/components/Button/Button.tsx
+++ apps/web/src/components/Button/Button.tsx
@@ -9,24 +9,26 @@ interface AsyncListener {
   (): Promise<void>
 }
 
-function Button(props: {children: string, buttonType: string, textType: string, destination?: string, listener?: Listener, asyncListener?: AsyncListener, style?: object}) {
+function Button(props: {children: string, buttonType: string, textType: string, destination?: string, listener?: Listener, asyncListener?: AsyncListener, style?: object, className?: string}) {
+  const { className, ...restProps } = props;
+
   if(props.listener) {
     const listener = props.listener ?? function() {return}
-    return <div className={"button " + props.buttonType} onClick={() => listener()} style={{...props.style}}><p className={props.textType} style={{margin: 0, ...props.style}}>{props.children}</p></div>
+    return <div className={"button " + props.buttonType + " " + className} onClick={() => listener()} style={{...props.style}}><p className={props.textType} style={{margin: 0, ...props.style}}>{props.children}</p></div>
   }
   if(props.asyncListener) {
     const asyncListener = props.asyncListener ?? function() {return}
-    return <div className={"button " + props.buttonType} onClick={() => asyncListener()} style={{...props.style}}><p className={props.textType} style={{margin: 0, ...props.style}}>{props.children}</p></div>
+    return <div className={"button " + props.buttonType + " " + className} onClick={() => asyncListener()} style={{...props.style}}><p className={props.textType} style={{margin: 0, ...props.style}}>{props.children}</p></div>
   }
   if(props.destination) {
     const destination = props.destination ?? "";
     return (
-      <Link to={destination} className={props.textType} style={{textDecoration: "none"}}>
-        <div className={"button " + props.buttonType} >{props.children}</div>
+      <Link to={destination} className={props.textType + " " + className} style={{textDecoration: "none"}}>
+        <div className={"button " + props.buttonType + " " + className} {...restProps}>{props.children}</div>
       </Link>
     );
   }
-  return <div className={"button " + props.buttonType} style={{...props.style}}><p className={props.textType} style={{margin: 0, ...props.style}}>{props.children}</p></div>
+  return <div className={"button " + props.buttonType + " " + className} style={{...props.style}}><p className={props.textType} style={{margin: 0, ...props.style}}>{props.children}</p></div>
 }
 
 export default Button;
--- apps/web/src/scenes/Home/Home.tsx
+++ apps/web/src/scenes/Home/Home.tsx
@@ -11,9 +11,13 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { BotMessageSquare } from "lucide-react";
+import {useState} from "react"
+import { Textarea } from "@/components/ui/textarea";
 
 function Home() {
   const {isLoading, error} = useAuth0();
+  const [promt, setPromt] = useState("")
+  const [answer, setAnswer] = useState("Notthing")
 
   if(isLoading) { return <p>loading your data</p>}
 
@@ -35,10 +39,27 @@ function Home() {
       </div>
       <div className="w-full h-full">
       <Popover >
-        <PopoverTrigger className="bg-teal-500 rounded-lg fixed top-auto bottom-10 left-auto right-12 p-4 flex gap-3">
-          <BotMessageSquare /> Chat with me
+        <PopoverTrigger className="bg-blue-950 rounded-lg fixed top-auto bottom-10 left-auto right-12 p-4 flex gap-3">
+          <BotMessageSquare className="text-gray-100" /> 
+          <span className="text-gray-100">ChatBot</span>
         </PopoverTrigger>
-        <PopoverContent className="bg-yellow-300">Place content for the popover here.</PopoverContent>
+        <PopoverContent className="bg-yellow-300 ">
+          <div className="h-auto w-full p-6">
+            <div className="user text-end">
+              123
+            </div>
+            <br />
+            <div className="bot">
+              xyz
+            </div>
+          </div>
+          <div className="flex gap-3 items-center">
+            <Textarea  placeholder="Aks anythings!" className="" onChange={(event) => setPromt(event.target.value)} />
+            {/* <Button buttonType='primary' textType="inverted-text-color" className="text-slate-950 bg-blue-500 rounded-lg">Send</Button> */}
+            <span>{promt}</span>
+          </div>
+          
+        </PopoverContent>
       </Popover>
       </div>
       <PromotionBar />
